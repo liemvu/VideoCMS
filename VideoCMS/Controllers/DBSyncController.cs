19246b83f537d4bf2781ab2e07da5b1bf6fb00dc
liem.vo <liemvouy@gmail.com>
2012-03-13 19:02:54 +0700

    first commit

---
+++ VideoCMS/Controllers/DBSyncController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoCMS.Controllers
+{
+    public class DBSyncController : Controller
+    {
+        //
+        // GET: /DBSync/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetDb(string name)
+        {
+            if (name == "cbv")
+            {
+                return new RedirectResult("http://dl.dropbox.com/u/17484775/movies-db/cbv-db.json");
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ VideoCMS/Controllers/ReportBadLinkController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoCMS.Controllers
+{
+    public class ReportBadLinkController : Controller
+    {
+        //
+        // GET: /ReportBadLink/
+
+        public ActionResult Index(string name, int id)
+        {
+            return null;
+        }
+
+    }
+}
