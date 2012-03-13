2ad5ed747529edb04a267556d94525247693c540
liem.vo <liemvouy@gmail.com>
2012-03-13 19:15:10 +0700

    test

--- VideoCMS/Controllers/HomeController.cs
+++ VideoCMS/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace VideoCMS.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC@!";
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
 
             return View();
         }
